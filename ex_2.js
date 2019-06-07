98ac636b0615087d8ac800c55c378d8056a61849
NatalyaDol <suhoikova@gmail.com>
2019-06-07 13:03:01 +0300

    Add the exersize  #2

--- ex_2.js
+++ ex_2.js
@@ -8,6 +8,10 @@
 //     input - входные параметры, по которым было произведено вычисление.
 
 'use strict';
+
+var form = document.querySelector("form"); // доступ к форме
+var areaFigure = document.getElementById("area_figure");// поле для вывода текста с информацией о площади
+
 class Figure {
     constructor(figure, par1, par2) {
         this.figure = figure;
@@ -15,7 +19,7 @@ class Figure {
         this.par2 = par2;
     }
 
-    calculateArea(figure, par1, par2) {
+    calculateArea(figure, par1, par2) { 
         let input = [par1, par2];
         let area;
         let obj;        
@@ -29,23 +33,27 @@ class Figure {
             obj = { area, figure, input };
 
         } else if (figure == "circle") {
-            area = input[0] * 3.1415;
+            area = Math.pow(input[0], 2) * 3.1415;
             obj = { area, figure, input };
 
         } 
         console.log(obj);  
-        console.log(`Yours figure is ${figure} with the parameters ${input} and the area ${area}`)
+        // console.log(`Yours figure is ${figure} with the parameters ${input} and the area ${area}`);
+        areaFigure.innerText = `Ваша фигура ${figure} c параметрами ${input} и площадью ${area}`
         
     };
    
 };
 
-var rectangle = new Figure;
-console.log(rectangle.calculateArea('rectangle', 20, 30));
+//Запуск расчета площади по объекту по параметрам пользователя, после клика на кнопку "посчитать площадь фигуры"
+function newFigureFunc() {
+    var figure = new Figure;
+    let figureName = form.elements[0].value;
+    let par1 = form.elements[1].value;
+    let par2 = form.elements[2].value;
 
-var triangle = new Figure;
-console.log(triangle.calculateArea('triangle', 20, 30));
-
-var triangle = new Figure;
-console.log(triangle.calculateArea('circle', 20));
-
+    figure.calculateArea(figureName, par1, par2);
+    // console.log(form.elements[0].value);
+    // console.log(form.elements[1].value);
+    // console.log(form.elements[2].value);
+};
